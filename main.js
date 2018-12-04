51ead45fac4339381b4ae4d76431d275ad546396
Sven van Hal <sven@svenvanhal.nl>
2018-12-04 14:04:01 +0100

    Add better map of the Netherlands, wireframe, interactive example.

--- main.js
+++ main.js
@@ -8,30 +8,32 @@ let
   projection
 ;
 
+let stations = require("./knmi/stations")
+
 
 const resize = () => {
   let container = document.getElementById('viscontainer');
   viewWidth = container.clientWidth;
   viewHeight = container.clientHeight;
-  
-  vis = vis
-    .attr('width', viewWidth)
-    .attr('height', viewHeight);
+  //
+  // vis = vis
+  //   .attr('width', viewWidth)
+  //   .attr('height', viewHeight);
 };
 
 const initMap = (features, config) => {
   const m = config.map;
   const f = config.features;
-  
+
   // Mercator projection is worldmap on a square
   projection = d3.geoMercator()
     .scale(m.scale)
     .center(m.center)
     .translate(m.translate);
-  
+
   let path = d3.geoPath()
     .projection(projection);
-  
+
   // create group or use existing one
   let map = d3.select('#map');
   if (map.empty()) {
@@ -39,7 +41,7 @@ const initMap = (features, config) => {
       .append('g')
       .attr('id', m.id);
   }
-  
+
   // features paths
   map.selectAll('path')
     .data(features)
@@ -54,19 +56,28 @@ const initMap = (features, config) => {
       d3.select(nodes[i]).classed('hover', false)
     })
     .on('click', (d) => {
-      console.log(d[f.props][f.text]);
-      console.log(d[f.props]);
+	  let el = document.getElementById("example");
+
+	  let html = `<h3>${d.properties.statnaam}</h3><p>Stations in this (land) area:</p><ul>`;
+
+	  // Find stations in this province:
+	  for (let station of stations) {
+	    if (d3.geoContains(d.geometry, [station.lon, station.lat])) html += `<li>${station.name}</li>`;
+	  }
+
+	  html += "</ul>";
+	  el.innerHTML = html;
     });
 };
 
 const start = () => {
   resize();
   d3.select(window).on('resize', resize);
-  
+
   // load data, requires hardcoded string or parcel wont bundle it
-  let data = require('./geoJSON/cbs_2013');
+  let data = require('./geoJSON/provincie_2017');
   let features = data.features;
-  
+
   let config = {
     map: {
       id: 'map',
@@ -80,27 +91,13 @@ const start = () => {
       text: 'areaName'
     }
   };
+
   initMap(features, config);
-  
-  
-  // example of lat/long
-  let cities = [
-    {
-      name: 'Amsterdam',
-      lon: '4.899431',
-      lat: '52.379189'
-    },
-    {
-      name: 'Delft',
-      lon: '4.3570677',
-      lat: '52.0115769'
-    }
-  ];
-  
+
   let points = vis.append('g');
   points
     .selectAll('circle')
-    .data(cities)
+    .data(stations)
     .enter()
     .append('circle')
     .attr('cx', (d) => projection([d.lon, d.lat])[0])
@@ -109,5 +106,4 @@ const start = () => {
     .style('fill', 'red');
 };
 
-// document.addEventListener('DOMContentLoaded', () => start());
 start();
